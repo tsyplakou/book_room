abedd652e349a7421067b711e064748a4d041996
danil_proskurin <danielpashtetov@gmail.com>
2024-12-06 00:51:54 +0700

    add custom filters for room and room_booking

---
+++ room/filters.py
@@ -0,0 +1,21 @@
+import django_filters
+from .models import Room
+
+
+class RoomFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name="name", lookup_expr="icontains", label="Room Name"
+    )
+    location_country = django_filters.CharFilter(
+        field_name="location__country", lookup_expr="icontains", label="Country"
+    )
+    location_city = django_filters.CharFilter(
+        field_name="location__city", lookup_expr="icontains", label="City"
+    )
+    capacity = django_filters.NumberFilter(
+        field_name="capacity", lookup_expr="gte", label="Minimum Capacity"
+    )
+
+    class Meta:
+        model = Room
+        fields = ['capacity', 'name', 'location_country', 'location_city']
