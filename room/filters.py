5d684ef2f439bf5f630f03f54eed6057e190f441
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-12-05 19:42:17 +0100

    Fix filters

--- room/filters.py
+++ room/filters.py
@@ -7,15 +7,15 @@ class RoomFilter(django_filters.FilterSet):
         field_name="name", lookup_expr="icontains", label="Room Name"
     )
     location_country = django_filters.CharFilter(
-        field_name="location_country", lookup_expr="icontains", label="Country"
+        field_name="location__country", lookup_expr="icontains", label="Country"
     )
     location_city = django_filters.CharFilter(
-        field_name="location_city", lookup_expr="icontains", label="City"
+        field_name="location__city", lookup_expr="icontains", label="City"
     )
-    capacity = django_filters.NumberFilter(
+    capacity_from = django_filters.NumberFilter(
         field_name="capacity", lookup_expr="gte", label="Minimum Capacity"
     )
 
     class Meta:
         model = Room
-        fields = ['capacity', 'name', 'location_country', 'location_city']
+        fields = ['capacity_from', 'name', 'location_country', 'location_city']
