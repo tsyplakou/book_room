39b8a5200f390bcc0cfece7d81760c5302a16541
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-12-05 17:49:57 +0100

    Add basic permissions

---
+++ book_room/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class AdminOrClientReadOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_admin or (
+                request.user.is_client and
+                request.method in SAFE_METHODS
+            )
+        )
+
+
+class AdminOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_admin
