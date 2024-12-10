f5005b9d0fb812855b1e3bb2b4c49a2dae06dc5f
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-12-10 20:01:04 +0100

    Fix permission check

--- book_room/permissions.py
+++ book_room/permissions.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class AdminOrClientReadOnlyPermission(BasePermission):
     def has_permission(self, request, view):
         return (
+            request.user and
+            request.user.is_authenticated
+        ) and (
             request.user.is_admin or (
                 request.user.is_client and
                 request.method in SAFE_METHODS
