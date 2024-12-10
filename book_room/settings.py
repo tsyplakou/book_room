2da66a2e9fae24aee4b4cbaf54ab6941b423c9ec
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-12-10 20:04:20 +0100

    Add settings for emailing using django

--- book_room/settings.py
+++ book_room/settings.py
@@ -190,3 +190,10 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
