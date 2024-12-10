37119ea60e6b897424dab8a93c21ac30f966d268
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-12-10 20:02:53 +0100

    Add more examples for pupils

--- room_booking/views.py
+++ room_booking/views.py
@@ -1,8 +1,20 @@
-from rest_framework import viewsets, exceptions
+from datetime import datetime, timedelta
+from django.core.mail import send_mail
 
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, exceptions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.conf import settings
+
+from room.models import Room
 from .filters import BookingFilter
 from .models import Booking
 from .serializers import BookingSerializer
+
+
+ROOM_OPENING_TIME = 9
+ROOM_CLOSING_TIME = 18
 
 
 class BookingViewSet(viewsets.ModelViewSet):
@@ -22,3 +34,54 @@ class BookingViewSet(viewsets.ModelViewSet):
         if obj.client != self.request.user:
             raise exceptions.NotFound
         return obj
+
+    @action(
+        detail=False,
+        url_path='available_hours/(?P<room>[0-9]+)/(?P<week_number>[0-9]+)',
+    )
+    def available_hours_for_room(self, request, room, week_number):
+        room = get_object_or_404(Room, pk=room)
+
+        start_date = datetime.fromisocalendar(
+            datetime.now().year,
+            int(week_number),
+            1
+        )
+        end_date = start_date + timedelta(days=7)
+
+        # room_bookings_on_the_week = room.bookings.filter(
+        #     Q(start_dt__gte=start_date, start_dt__lte=end_date) |
+        #     Q(end_dt__gte=start_date, end_dt__lte=end_date)
+        # )
+
+        available_hours = []
+        for day in range(1, 8):
+            date = start_date + timedelta(days=day - 1)
+            if room.bookings.filter(
+                start_dt__lte=date,
+                end_dt__gte=date,
+            ).exists():
+                continue
+            else:
+                if date.hour >= ROOM_OPENING_TIME and date.hour < ROOM_CLOSING_TIME:
+                    available_hours.append(date.strftime('%H:%M'))
+
+        return Response(available_hours)
+
+    @action(
+        detail=True,
+        methods=['post'],
+    )
+    def send_confirmation_mail(self, request, pk):
+        booking = self.get_object()
+
+        send_mail(
+            subject='Booking Confirmation',
+            message='',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[booking.client.email],
+            html_message='{} / {} / {}'.format(
+                booking.room.name, booking.start_dt, booking.end_dt
+            ),
+        )
+        return Response()
